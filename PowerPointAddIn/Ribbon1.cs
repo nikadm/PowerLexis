2365667ad0790679401cd5b4de17caac3ce3e206
nikadm <nikise02@mail.ru>
2013-11-08 00:00:22 +0400

    PowerLexis

---
+++ PowerPointAddIn/Ribbon1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Interop.PowerPoint;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace PowerPointAddIn
+{
+    public partial class Ribbon1
+    {
+        private Presentation newPreset;
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            newPreset = CreateNewPPT();
+        }
+
+        public Presentation CreateNewPPT()
+        {
+
+            var appPPT = new Application();
+            var pptPresent = appPPT.Presentations.Add();
+
+            var pptLayout = pptPresent.SlideMaster.CustomLayouts[PpSlideLayout.ppLayoutChartAndText];
+            var pptLayout1 = pptPresent.SlideMaster.CustomLayouts[PpSlideLayout.ppLayoutTitle];
+            var pptLayout2 = pptPresent.SlideMaster.CustomLayouts[PpSlideLayout.ppLayoutTable];
+
+            pptPresent.Slides.AddSlide((pptPresent.Slides.Count + 1), pptLayout);
+            
+            pptPresent.Slides.AddSlide((pptPresent.Slides.Count + 1), pptLayout1);
+            pptPresent.Slides.AddSlide((pptPresent.Slides.Count + 1), pptLayout2);
+
+            return pptPresent;
+        }
+
+        private void button2_Click(object sender, RibbonControlEventArgs e)
+        {
+            var pptLayout = newPreset.SlideMaster.CustomLayouts[PpSlideLayout.ppLayoutTitle];
+            newPreset.Slides.AddSlide((newPreset.Slides.Count + 1), pptLayout);
+        }
+
+       
+    }
+}
---
+++ PowerPointAddIn/ThisAddIn.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using PowerPoint = Microsoft.Office.Interop.PowerPoint;
+using Office = Microsoft.Office.Core;
+
+namespace PowerPointAddIn
+{
+    public partial class ThisAddIn
+    {
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            Ribbon1 newri=new Ribbon1();
+        }
+
+       
+
+        #region Код, автоматически созданный VSTO
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+           }
+        
+        #endregion
+    }
+}
